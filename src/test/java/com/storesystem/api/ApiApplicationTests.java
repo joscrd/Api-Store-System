cf4ed4304cf5d0f969135238bcd417ba7d57466d
joscrd <j194cc@gmail.com>
2024-06-07 00:36:20 -0700

    first commit

---
+++ src/test/java/com/storesystem/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storesystem.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
